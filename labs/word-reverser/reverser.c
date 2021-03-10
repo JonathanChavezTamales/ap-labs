e8138dc97c302741974ae69ac09512e82b1af4cf
Jonathan <jonathanchatab@hotmail.com>
2021-03-09 19:27:18 -0600

    Solve word-reverser

--- labs/word-reverser/reverser.c
+++ labs/word-reverser/reverser.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h> 
+
+int  append(char*s, char c) {
+     int len = strlen(s);
+     s[len] = c;
+     s[len+1] = '\0';
+     return 0;
+}
 
 int main(){
-    // Place your magic here
-    return 0;
+	char str[10000] = "";
+	char temp = 'a';
+
+	while(temp != EOF){
+		temp = getchar();
+		if(temp != '\n'){
+			append(str, temp);
+		}
+		else if(temp != EOF){
+			for(int i=strlen(str)-1; i>=0; i--){
+				printf("%c", str[i]);
+			}
+			printf("\n");
+			strcpy(str, "");
+		}
+
+	}
+    	return 0;
 }
